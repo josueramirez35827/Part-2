ae8586c2ef6bbb6a1fb30a3debccb56e43567b03
Josue Ramirez-Romero <josueramirez35827@gmail.com>
2022-06-08 21:26:55 -0400

    New Guesses

--- models/lambRecipe.js
+++ models/lambRecipe.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
+const OriginCountry = require("./originCountry");
+
+
 const lambRecipe = new Schema(
   {
     title: { type: String, required: true },
-    origin_country: { type: String, required: true },
+    origin_country: { type: Schema.Types.ObjectId, ref: "OriginCountry" },
     ingredients: { type: String, required: true },
     picture: { type: String, required: true },
   },
